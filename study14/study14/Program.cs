785dc7cd0f5da069d93ff5c3375ad80c82ea6115
yang491658 <park49164357@gmail.com>
2025-02-27 11:02:13 +0900

    250227 1102 - study14

---
+++ study14/study14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 네임 스페이스
+// 클래스, 함수, 변수 이름이 충돌하는 것을 방지하기 위해 사용된다.
+namespace dev1
+{
+    class MyClass
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("안녕하세요 !MyNameSpace의 MyClass입니다.");
+        }
+    }
+}
+namespace study14
+{
+    class Program
+    {
+        static void SayHello()
+        {
+            Console.WriteLine("반갑습니다.");
+        }
+
+        static void Main(string[] args)
+        {
+            SayHello();
+            dev1.MyClass.SayHello();
+        }
+    }
+}
