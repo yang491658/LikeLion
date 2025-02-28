d67e4193089e8c7bef28dd7d43f302fce3b34356
yang491658 <park49164357@gmail.com>
2025-02-28 09:30:19 +0900

    250228 0930 - study18

---
+++ study19/study19/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Current Date and Time : {now}"); // 현재 날짜 시간
+
+            TimeSpan duration = new TimeSpan(1, 30, 0); // 1시간 30분
+            Console.WriteLine($"Duration : {duration}");
+        }
+    }
+}
