b4c697573461b33def4db35eaeb010794eb8e8fe
yang491658 <park49164357@gmail.com>
2025-03-01 00:59:38 +0900

    250301 0059

---
+++ 250228/ShootingGame2/ShootingGame2/Program.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootingGame2
+{
+    // 미사일 클래스
+    public class Bullet
+    {
+        //public int x;
+        //public int y;
+        //public bool fire;
+
+        // 리팩토링
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsFired { get; set; } = false;
+    }
+
+    // 플레이어 클래스
+    public class Player
+    {
+        [DllImport("msvcrt.dll")]
+        static extern int _getch(); // C언어 함수 가져옴
+
+        // 플레이어 변수
+        //public int playerX;
+        //public int playerY;
+        //public Bullet[] playerBullet = new Bullet[20];
+        //public Bullet[] playerBullet2 = new Bullet[20];
+        //public Bullet[] playerBullet3 = new Bullet[20];
+        //public int Score = 100;
+        //public Item item = new Item();
+        //public int itemCount = 0;
+
+        // 리팩토링
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public List<Bullet> Bullets { get; } = new List<Bullet>();
+        public int Score { get; private set; } = 100;
+        public Item Item { get; } = new Item();
+        public int ItemCount { get; private set; } = 0;
+
+        // 생성자
+        public Player()
+        {
+            // 플레이어 좌표 초기화
+            //playerX = 0;
+            //playerY = 12;
+
+            // 미사일 초기화
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    playerBullet[i] = new Bullet();
+            //    playerBullet[i].x = 0;
+            //    playerBullet[i].y = 0;
+            //    playerBullet[i].fire = false;
+
+            //    playerBullet2[i] = new Bullet();
+            //    playerBullet2[i].x = 0;
+            //    playerBullet2[i].y = 0;
+            //    playerBullet2[i].fire = false;
+
+            //    playerBullet3[i] = new Bullet();
+            //    playerBullet3[i].x = 0;
+            //    playerBullet3[i].y = 0;
+            //    playerBullet3[i].fire = false;
+            //}
+
+            // 리팩토링
+            X = 0;
+            Y = 12;
+            for (int i = 0; i < 20; i++) Bullets.Add(new Bullet());
+        }
+
+        public void GameMain()
+        {
+            KeyControl(); // 키를 입력하는 부분
+            PlayerDraw(); // 플레이어 그리기
+            UIscore(); // UI점수
+
+            // 아이템 그리기
+            if (Item.ItemLife)
+            {
+                Item.ItemMove();
+                Item.ItemDraw();
+                CrashItem();
+            }
+        }
+
+        // 키 입력
+        public void KeyControl()
+        {
+            int pressKey; // 정수형 변수 선언 -> 키 값 입력
+
+            if (Console.KeyAvailable) // 키가 눌렸을때
+            {
+                pressKey = _getch(); // 아스키 값 : 입력한 키를 아스키 코드로 받음
+
+                if (pressKey == 224 || pressKey == 0) // 방향키 또는 확장 키
+                {
+                    pressKey = _getch(); // 실제 키 값 읽기
+                }
+
+                switch (pressKey)
+                {
+                    //case 72: // 위쪽 방향키 아스키 코드
+                    //    playerY--;
+                    //    if (playerY < 1) playerY = 1;
+                    //    break;
+                    //case 75: // 왼쪽 방향키
+                    //    playerX--;
+                    //    if (playerX < 0) playerX = 0;
+                    //    break;
+                    //case 77: // 오른쪽 방향키
+                    //    playerX++;
+                    //    if (playerX > 75) playerX = 75;
+                    //    break;
+                    //case 80: // 아래쪽 방향키
+                    //    playerY++;
+                    //    if (playerY > 22) playerY = 22;
+                    //    break;
+                    //case 32: // 스페이스바
+                    //    // 미사일 발사
+                    //    for (int i = 0; i < 20; i++)
+                    //    {
+                    //        // 미사일 false = 발사 가능
+                    //        if (playerBullet[i].fire == false)
+                    //        {
+                    //            // 미사일 true = 발사 중
+                    //            playerBullet[i].fire = true;
+
+                    //            // 플레이어 앞에서 미사일 쏘기
+                    //            playerBullet[i].x = playerX + 5;
+                    //            playerBullet[i].y = playerY;
+
+                    //            break; // 1발씩 발사
+                    //        }
+                    //    }
+                    //    break;
+                    case 72: Y = Math.Max(1, Y - 1); break;
+                    case 75: X = Math.Max(0, X - 1); break;
+                    case 77: X = Math.Min(75, X + 1); break;
+                    case 80: Y = Math.Min(22, Y + 1); break;
+                    case 32:
+                        Bullets.Add(new Bullet { X = X + 5, Y = Y + 1, IsFired = true });
+                        if (ItemCount >= 1) Bullets.Add(new Bullet { X = X + 5, Y = Y, IsFired = true });
+                        if (ItemCount >= 2) Bullets.Add(new Bullet { X = X + 5, Y = Y + 2, IsFired = true });
+                        break;
+                }
+            }
+        }
+
+        // 플레이어 그리기
+        public void PlayerDraw()
+        {
+            // 문자열 배열로 플레이어 그리기
+            string[] playerShape = new string[]
+            {
+                "->",
+                ">>>",
+                "->"
+            };
+
+            for (int i = 0; i < playerShape.Length; i++)
+            {
+                Console.SetCursorPosition(X, Y + i - 1); // 콘솔 좌표 설정
+                Console.WriteLine(playerShape[i]); // 문자열 배열 출력
+            }
+        }
+
+        // 미사일 그리기
+        public void BulletDraw()
+        {
+            // 미사일 형태
+            string bulletSymbol = "->";
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    // 미사일 true = 미사일이 살아있는 상태
+            //    if (playerBullet[i].fire == true)
+            //    {
+            //        // 좌표설정 -> 중간 위치를 위한 보정을 위해 x-1
+            //        Console.SetCursorPosition(playerBullet[i].x, playerBullet[i].y);
+
+            //        Console.Write(bullet); // 미사일 출력
+
+            //        playerBullet[i].x++; // 미사일 발사 : 오른쪽으로 이동
+
+            //        // 가장 오른쪽에 도달 시 -> 미사일 false = 다시 준비 상태
+            //        if (playerBullet[i].x > 78)
+            //        {
+            //            playerBullet[i].fire = false;
+            //        }
+            //    }
+            //}
+
+            // 리팩토링
+            foreach (var bullet in Bullets)
+            {
+                if (bullet.IsFired)
+                {
+                    Console.SetCursorPosition(bullet.X - 1, bullet.Y - 1);
+                    Console.Write(bulletSymbol);
+                    bullet.X++;
+                    if (bullet.X > 78) bullet.IsFired = false;
+                }
+            }
+        }
+
+        // 충돌처리
+        public void ClashEnemyAndBullet(Enemy enemy)
+        {
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    // 발사중인 미사일
+            //    if (playerBullet[i].fire == true)
+            //    {
+            //        // 미사일과 적의 y값이 같을 떄
+            //        if (playerBullet[i].y == enemy.enemyY)
+            //        {
+            //            // 충돌
+            //            if (playerBullet[i].x >= enemy.enemyX - 1
+            //                && playerBullet[i].x <= enemy.enemyX + 1)
+            //            {
+            //                // 아이템 생성
+            //                item.ItemLife = true;
+            //                item.itemX = enemy.enemyX;
+            //                item.itemY = enemy.enemyY;
+
+            //                Random rand = new Random();
+            //                enemy.enemyX = 75;
+            //                enemy.enemyY = rand.Next(2, 22);
+
+            //                playerBullet[i].fire = false; // 미사일도 준비 상태
+
+            //                Score += 100; // 스코어 상승
+            //            }
+            //        }
+            //    }
+            //}
+
+            // 리팩토링
+            foreach (var bullet in Bullets)
+            {
+                if (bullet.IsFired
+                    && bullet.Y - 1 == enemy.Y
+                    && bullet.X >= (enemy.X - 1)
+                    && bullet.X <= (enemy.X + 1))
+                {
+                    Item.ItemLife = true;
+                    Item.X = enemy.X;
+                    Item.Y = enemy.Y;
+
+                    Random rand = new Random();
+                    enemy.X = 75;
+                    enemy.Y = rand.Next(2, 22);
+
+                    bullet.IsFired = false;
+                    Score += 100;
+                }
+            }
+        }
+
+        public void UIscore()
+        {
+            Console.SetCursorPosition(63, 0);
+            Console.Write("┏━━━━━━━━━━━━━━┓");
+            Console.SetCursorPosition(63, 1);
+            Console.Write("┃ Score : " + Score + "  ┃");
+            Console.SetCursorPosition(63, 2);
+            Console.Write("┗━━━━━━━━━━━━━━┛");
+        }
+
+        // 아이템 충돌 시 미사일 수 증가
+        public void CrashItem()
+        {
+            //if (playerY >= item.itemY - 1 && playerY <= item.itemY + 1)
+            //{
+            //    if (playerX >= item.itemX - 2 && playerX <= item.itemX + 2)
+            //    {
+            //        item.ItemLife = false;
+
+            //        if (itemCount < 3) itemCount++;
+
+            //        //총알 초기화
+            //        for (int i = 0; i < 20; i++)
+            //        {
+            //            playerBullet[i] = new Bullet();
+            //            playerBullet[i].x = 0;
+            //            playerBullet[i].y = 0;
+            //            playerBullet[i].fire = false;
+            //        }
+            //    }
+            //}
+
+            if (Y >= Item.Y - 1 && Y <= Item.Y + 1
+                && X >= Item.X - 2 && X <= Item.X + 2)
+            {
+                Item.ItemLife = false;
+                ItemCount = Math.Min(3, ItemCount + 1);
+            }
+        }
+    }
+
+    // 적 클래스
+    public class Enemy
+    {
+        //public int enemyX;
+        //public int enemyY;
+
+        // 리팩토링
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        // 적 좌표 초기화
+        public Enemy()
+        {
+            X = 75;
+            Y = 12;
+        }
+
+        public void EnemyDraw()
+        {
+            string enemyShape = "<-0->"; // 적을 문자열로 표현
+            Console.SetCursorPosition(X, Y); // 좌표 설정
+            Console.Write(enemyShape);
+        }
+
+        public void EnemyMove()
+        {
+            Random rand = new Random();
+
+            X--; // 왼쪽으로 이동
+
+            // 가장 왼쪽 도달 시, 새로운 적 생성
+            if (X < 2)
+            {
+                X = 75;
+                Y = rand.Next(2, 22);
+            }
+        }
+    }
+
+    // 아이템 클래스
+    public class Item
+    {
+        public string ItemName;
+        public string ItemShape;
+        //public int itemX = 0;
+        //public int itemY = 0;
+        //public bool ItemLife = false;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool ItemLife { get; set; } = false;
+
+        // 아이템 그리기
+        public void ItemDraw()
+        {
+            Console.SetCursorPosition(X, Y);
+            ItemShape = "Item★";
+            Console.Write(ItemShape);
+        }
+
+        // 아이템 이동
+        public void ItemMove()
+        {
+            X--;
+            if (X <= 1 || Y <= 1) ItemLife = false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+
+            Player player = new Player(); // 플레이어 생성
+            Enemy enemy = new Enemy(); // 적 생성
+
+            // 유니티처럼 속도를 프레임속도로
+            int dwTime = Environment.TickCount; // 1ms
+
+            // 무한 반복
+            while (true)
+            {
+                // 0.05초 (50ms) 지연
+                if (dwTime + 50 < Environment.TickCount)
+                {
+                    // 현재시간 세팅
+                    dwTime = Environment.TickCount;
+                    Console.Clear();
+
+                    // 게임 시작
+                    player.GameMain();
+
+                    // 미사일
+                    //if (player.itemCount == 0)
+                    //{
+                    player.BulletDraw();
+                    //}
+                    //else if (player.itemCount == 1)
+                    //{
+                    //    player.BulletDraw();
+                    //    player.BulletDraw2();
+                    //}
+                    //else
+                    //{
+                    //    player.BulletDraw();
+                    //    player.BulletDraw2();
+                    //    player.BulletDraw3();
+                    //}
+
+                    // 적
+                    enemy.EnemyMove(); // 이동
+                    enemy.EnemyDraw(); // 그리기
+
+                    // 충돌처리
+                    player.ClashEnemyAndBullet(enemy);
+                }
+            }
+        }
+    }
+}
+
---
+++ 250228/TodayTask250228/TodayTask250228/Program.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TodayTask250228
+{
+    // 게임 화면 클래스
+    public class Game
+    {
+        // 전역 변수
+        public static int w = 80; // 가로
+        public static int h = 25; // 세로
+        public static int hm = 6; // 하단 여백
+        public static int frame = 15; // 프레임
+        public Player player = new Player();
+        public Item item = new Item();
+
+        // 제목 변수
+        private static string title1 = "SHOOTING";
+        private static string title2 = "GAME";
+        private static string[] logo =
+        {
+            " _____",
+            "||   ||",
+            "||   ||",
+            "|│___||",
+            "|/___\\|",
+        };
+
+        // 시작 화면
+        public static void Start()
+        {
+            // 상하 여백
+            int marginY = h / 10;
+
+            PrintSide();
+            PrintTile(title1, marginY);
+            PrintTile(title2, h - marginY - logo.Length);
+
+            while (!Console.KeyAvailable)
+            {
+                Console.SetCursorPosition((w - 37) / 2, h / 2);
+                Console.WriteLine("아무 키나 누르시면 게임이 시작됩니다.");
+            }
+        }
+
+        // 진행 화면
+        public static void View(Player player, Item item)
+        {
+            PrintSide();
+            player.Draw();
+            player.InputKey();
+            if (item.isExist)
+            {
+                item.Draw();
+                item.Move();
+            }
+            player.Get(item);
+        }
+
+        // 제목 출력
+        private static void PrintTile(string title, int initY)
+        {
+            // 변수
+            int box = 8; // 박스 크기
+            int marginX = w / 10; // 좌우 여백
+            int count = title.Length; // 제목 글자 수
+            int invX = (w - 2 - 2 * marginX - count * box) / (count - 1); // 제목 간격
+
+            // 각 제목 글자 출력
+            for (int i = 0; i < title.Length; i++)
+            {
+                // 제목 박스
+                for (int j = 0; j < logo.Length; j++)
+                {
+                    Console.SetCursorPosition(1 + marginX + i * (invX + box), initY + j);
+                    Console.WriteLine(logo[j]);
+                    Thread.Sleep(300 / frame);
+                }
+
+                // 제목 글자
+                Console.SetCursorPosition(box / 2 + marginX + i * (invX + box), initY + 2);
+                Console.WriteLine(title[i]);
+            }
+        }
+
+        // 테두리 출력
+        public static void PrintSide()
+        {
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    if (x == 0 && y == 0) Console.Write("┏");
+                    else if (x == w - 1 && y == 0) Console.Write("┓");
+                    else if (x == 0 && y == h - 1) Console.Write("┗");
+                    else if (x == w - 1 && y == h - 1) Console.Write("┛");
+                    else if (x == 0 || x == w - 1) Console.Write("┃");
+                    else if (y == 0 || y == h - 1) Console.Write("━");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    // 플레이어 클래스
+    public class Player
+    {
+        // 플레이어 변수
+        public int x { get; private set; }
+        public int y { get; private set; }
+        public int maxX { get; private set; }
+        public int maxY { get; private set; }
+        public int score { get; private set; } = 100;
+
+        public List<Bullet> bullets { get; } = new List<Bullet>();
+        public int shootTime = Environment.TickCount;
+
+        public List<Item> itemList { get; set; } = new List<Item>();
+
+        // 플레이어 생성자
+        public Player()
+        {
+            // 플레이어 좌표
+            x = 1;
+            y = Game.h / 2;
+            maxX = Game.w - 7;
+            maxY = Game.h - 2;
+
+            // 총알
+            for (int i = 0; i < 20; i++) bullets.Add(new Bullet());
+        }
+
+        // 키 입력
+        public void InputKey()
+        {
+            // 키가 눌렸을 때
+            if (Console.KeyAvailable)
+            {
+                // 입력된 키 정보 가져오기
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    // 방향키 -> 플레이어 이동
+                    case ConsoleKey.LeftArrow: x = Math.Max(1, x - 1); break;
+                    case ConsoleKey.RightArrow: x = Math.Min(maxX, x + 1); break;
+                    case ConsoleKey.UpArrow: y = Math.Max(1, y - 1); break;
+                    case ConsoleKey.DownArrow: y = Math.Min(maxY, y + 1); break;
+                    // 스페이스바 -> 총알 발사 (0.5초 당 1번)
+                    case ConsoleKey.Spacebar:
+                        if (shootTime + 500 <= Environment.TickCount)
+                        {
+                            bullets.Add(new Bullet { x = x + 5, y = y, shoot = true });
+                            shootTime = Environment.TickCount;
+                        }
+                        break;
+                }
+            }
+        }
+
+        // 플레이어 그리기
+        public void Draw()
+        {
+            // 플레이어 형태
+            string shape = "착한놈";
+
+            // 플레이어 출력
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(shape);
+        }
+
+        // 총알 발사
+        public void Shoot()
+        {
+            // 총알 형태
+            string shape = "슛";
+
+            // 총알 그리기
+            foreach (var bullet in bullets)
+            {
+                // 총알 발사 중
+                if (bullet.shoot)
+                {
+                    // 총알 출력
+                    Console.SetCursorPosition(bullet.x, bullet.y);
+                    Console.Write(shape);
+
+                    // 총알 이동
+                    bullet.x++;
+                    // 총알 우측 이동 -> 가장 우측 도달 시, 삭제
+                    if (bullet.x > Game.w - 3) bullet.shoot = false;
+                }
+            }
+        }
+
+        // 적 처치
+        public void Kill(Enemy enemy, Item item)
+        {
+            foreach (var bullet in bullets)
+            {
+                // 총알 발사 및 충돌
+                if (bullet.shoot
+                    && bullet.y == enemy.y
+                    && bullet.x >= enemy.x - 1
+                    && bullet.x <= enemy.x + 7)
+                {
+                    Random rand = new Random();
+
+                    // 아이템 랜덤 생성
+                    if (!item.isExist)
+                    {
+                        item.isExist = true;
+                        // 가장 상단 -> 아래로 이동
+                        if (enemy.y == 1)
+                            item.moveUp = false;
+                        // 가장 하단 -> 위로 이동
+                        else if (enemy.y == maxY)
+                            item.moveUp = true;
+                        else
+                            item.moveUp = rand.Next(2) == 0;
+
+                        // 아이템 좌표 = 죽은 적 좌표
+                        item.x = enemy.x;
+                        item.y = enemy.y;
+                    }
+
+                    // 새로운 적 좌표
+                    enemy.x = Game.w - 7;
+                    enemy.y = rand.Next(1, Game.h - 1);
+
+                    // 충돌된 총알 제거
+                    bullet.shoot = false;
+
+                    // 점수 상승
+                    score += 100;
+                }
+            }
+        }
+
+        // 아이템 획득
+        public void Get(Item item)
+        {
+            if (item.y == y
+                && item.x >= x - 2
+                && item.x <= x + 5)
+            {
+                item.isExist = false;
+            }
+        }
+    }
+
+    // 총알 클래스
+    public class Bullet
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public bool shoot { get; set; } = false;
+    }
+
+    // 적 클래스
+    public class Enemy
+    {
+        // 적 변수
+        public string shape { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+
+        // 적 초기화
+        public Enemy()
+        {
+            // 적 형태
+            shape = "나쁜놈";
+            // 적 좌표
+            x = Game.w - 7;
+            y = Game.h / 2;
+        }
+
+        // 적 그리기
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(shape);
+        }
+
+        // 적 이동
+        public void Move()
+        {
+            Random rand = new Random();
+
+            // 적 왼쪽 이동 -> 가장 좌측 도달 시, 새로운 적 생성
+            if (--x < 2)
+            {
+                x = Game.w - 7;
+                y = rand.Next(1, Game.h - 1);
+            }
+        }
+    }
+
+    // 아이템 클래스
+    public class Item
+    {
+        // 아이템 변수
+        public string shape;
+        public int x { get; set; }
+        public int y { get; set; }
+        public bool moveLeft { get; set; } = true;
+        public bool moveUp { get; set; } = true;
+        public bool isExist { get; set; } = false;
+
+        // 아이템 그리기
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            shape = "폭탄";
+            Console.Write(shape);
+        }
+
+        // 아이템 이동
+        public void Move()
+        {
+            // 아이템 상하 이동 -> 가장 상단/하단 도달 시, 방향 전환
+            if (moveUp) y--; else y++;
+            if (y <= 1 || y >= Game.h - 2) moveUp = !moveUp;
+
+            // 아이템 좌우 이동 -> 가장 좌측/우측 도달 시, 방향 전환
+            if (moveLeft) x -= 2; else x += 2;
+            if (x <= 2 || x >= Game.w - 5) moveLeft = !moveLeft;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 콘솔 설정
+            Console.CursorVisible = false;
+            Console.SetWindowSize(Game.w, Game.h + Game.hm);
+            Console.SetBufferSize(Game.w, Game.h + Game.hm);
+            int dwTime = Environment.TickCount; // 1ms
+
+            Game.Start();
+
+            Player player = new Player(); // 플레이어 생성
+            Enemy enemy = new Enemy(); // 적 생성
+            Item item = new Item(); // 아이템 생성
+
+            while (true)
+            {
+                // 지연
+                if (dwTime + 1000 / Game.frame < Environment.TickCount)
+                {
+                    // 현재시간 세팅
+                    dwTime = Environment.TickCount;
+                    Console.Clear();
+
+                    // 게임 화면
+                    Game.View(player, item);
+
+                    // 총알 발사
+                    player.Shoot();
+
+                    // 적
+                    enemy.Move(); // 이동
+                    enemy.Draw(); // 그리기
+
+                    // 처치
+                    player.Kill(enemy, item);
+                }
+            }
+        }
+    }
+}
