75def2c0b1bd8ce0b3d98656a3f41ce1335534c7
yang491658 <park49164357@gmail.com>
2025-03-06 08:13:07 +0900

    250306 0813

--- 250221/study6ReadLine/study6ReadLine/Program.cs
+++ 250221/study6ReadLine/study6ReadLine/Program.cs
@@ -11,7 +11,7 @@ namespace LikeLion6ReadLine
         static void Main(string[] args)
         {
             //// 사용자 입력을 문자열로 받기
-            //Console.Write("이르을 입력하세요 : ");
+            //Console.Write("이름을 입력하세요 : ");
             //string userName = Console.ReadLine(); // 사용자로부터 입력받기
             //Console.WriteLine($"안녕하세요, {userName}님!"); // 입력값 출력
 
