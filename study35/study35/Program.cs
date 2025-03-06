df9fbe54adbfaf104dd6dce4fcb754eff2aec33d
yang491658 <park49164357@gmail.com>
2025-03-06 09:22:30 +0900

    250306 0922 - study35

---
+++ study35/study35/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class Say
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("안녕하세요!");
+        }
+    }
+}
+namespace study35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello.Say sa = new Hello.Say();
+            sa.SayHello();
+        }
+    }
+}
