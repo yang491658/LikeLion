210ed6f6e99876e1b05df549eafb3969d3a43b0d
yang491658 <park49164357@gmail.com>
2025-03-06 15:37:42 +0900

    250306 1537 - InterfaceExam2

---
+++ InterfaceExam2/InterfaceExam2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExam2
+{
+    // 인터페이스를 사용하면 기존 코드 변경 없이 새로운 기능 추가가 가능함.
+    // 예) 다양한 결제 시스템 추가(Open-Closed Principle)
+
+    interface Payment
+    {
+        void ProcessPayment();
+    }
+
+    class CreditCard : Payment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("신용카드 결제 완료");
+        }
+    }
+
+    class PayPal : Payment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("페이팔 결제 완료");
+        }
+    }
+
+    class PaymentProcessor
+    {
+        public void Pay(Payment payment)
+        {
+            payment.ProcessPayment();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor processor = new PaymentProcessor();
+
+            Payment creditcard = new CreditCard();
+            Payment paypal = new PayPal();
+
+            processor.Pay(creditcard);
+            processor.Pay(paypal);
+        }
+    }
+}
